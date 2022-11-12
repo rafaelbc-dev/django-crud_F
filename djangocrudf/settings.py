c7095392bd6592e54b16ccd242e88d6b9f5e749d
rafaelbc-dev <rafaelbc.dev@gmail.com>
2022-11-12 18:10:51 +0100

    Settings modified.

--- djangocrudf/settings.py
+++ djangocrudf/settings.py
@@ -22,8 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-4km$_ar+#sf9x%tnbn+00)2_*tmc1if83-r_@c@bm)nc_2o--%'
-SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
+SECRET_KEY = os.environ.get('SECRET_KEY', default='django-insecure-4km$_ar+#sf9x%tnbn+00)2_*tmc1if83-r_@c@bm)nc_2o--%')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
